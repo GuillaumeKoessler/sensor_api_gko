968f45e1c7cec4656d3ac9cd1013b8479ed69c15
GuillaumeKoessler <guillaume.koessler@gmail.com>
2024-08-18 21:30:56 +0200

    initialize api

---
+++ app.py
@@ -0,0 +1,13 @@
+from datetime import date
+
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+
+app = FastAPI()
+
+
+@app.get("/")
+def visit(
+    store_name: str, year: int, month: int, day: int, sensor_id: int | None = None
+) -> JSONResponse:
+    return JSONResponse(status_code=200, content=f"Yay {store_name}")
